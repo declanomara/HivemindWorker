f7cc02d5db185d18bf4e804154bb0d7b6e7fa547
declan <25646795+declanomara@users.noreply.github.com>
2022-07-03 10:32:43 -0400

    Initial version of install.lua

--- install.lua
+++ install.lua
@@ -1,3 +1,4 @@
+local io = require("io")
 local filesystem = require("filesystem")
 local internet = require("internet")
 
@@ -7,13 +8,15 @@ local main_url = "https://github.com/declanomara/HivemindWorker/blob/master/main
 
 
 function download_script (url, save_name)
+    print('Downloading file "'..save_name..'"...')
     local handle = internet.request(url)
     local result = ""
     for chunk in handle do result = result..chunk end
 
-    local file = filesystem.open(save_name)
-    file.write(result)
-    file.close()
+    print('Saving file "'..save_name..'"...')
+    local file = io.open(save_name, "w")
+    file:write(result)
+    file:close()
 
     return True
 end
